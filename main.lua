2b18c936131e70e83f00ec624c34c23d6f027f57
Ben <mortimermcmiresa@gmail.com>
2021-05-27 21:15:56 -0400

    V2.0 upload

--- main.lua
+++ main.lua
@@ -18,10 +18,16 @@ if (mwse.buildDate == nil or mwse.buildDate < 20191220) then
 end
 
 local configPath = "mortLeveling.config"
-local config = mwse.loadConfig("mortLeveling") or {
+
+local config = mwse.loadConfig("mortLeveling", {
     majorSkillRate = 50,
     minorSkillRate = 50,
     modEnabled = true,
+	skillDecay = false,
+	skillDecayMessage = true,
+	skillDecayUseBase = false,
+	skillDecayMin = 15,
+	skillDecayTime = 15,
     attributeMaximum = 200,
 	luckPerLevel = 10,
     acrobaticsMod = 50,
@@ -32,7 +38,10 @@ local config = mwse.loadConfig("mortLeveling") or {
     miscSkillRate=30,
     majorSkillThreshold=0,
 	flatHealthPerLevel = 5,
-}
+})
+
+local majors
+local minors
 
 local levelUpMessage = {
 	[2] = "You realize that all your life you have been coasting along as if you were in a dream. Suddenly, facing the trials of the last few days, you have come alive.",
@@ -67,52 +76,78 @@ local function setLevel(ref, lvl)
     end
 end
 
+local function drainSkill(skillCode,amount)
+	local skillName = tes3.getSkill(skillCode).name
+	if config.skillDecayMessage then
+		tes3.messageBox("You have lost " .. skillName)
+	end
+	tes3.modStatistic{reference=tes3.player,skill=skillCode,value=amount}
+	--tes3.modStatistic{reference=tes3.player,attribute=tes3.getSkill(skillCode).attribute,value=-1}
+end
+
+local function setInitialStats()
+	
+end
+
 local function incrementSkill(skill,amount)
 	local path = tes3.player.data.mortLeveling
 	
-	if skill == 1 or skill == 4 or skill == 5 or skill == 6 then --strength
-		path.strengthProgress = (path.strengthProgress + amount)
-		if path.strengthProgress >= 1.0 and tes3.mobilePlayer.strength.base < config.attributeMaximum then
-			path.strengthProgress = (path.strengthProgress - 1)
-			tes3.modStatistic{reference=tes3.player,attribute=0,value=1}
-			tes3.messageBox("Your strength has improved.")
+	if skill == 777 then --luck special case
+		path.luckProgress = (path.luckProgress + amount)
+		if path.luckProgress >= 1.0 and tes3.mobilePlayer.luck.base < config.attributeMaximum then
+			path.luckProgress = (path.luckProgress - 1)
+			tes3.modStatistic{reference=tes3.player,attribute=7,value=1}
 		end
-	elseif skill == 20 then --acrobatics special case
+		return
+	end
+	
+	if tes3.attributeName[tes3.getSkill(skill).attribute] == nil then
+		return
+	end
+	
+	if skill == 20 then --acrobatics special case
 		path.strengthProgress = (path.strengthProgress + (config.acrobaticsMod*0.01)*amount)
 		if path.strengthProgress >= 1.0 and tes3.mobilePlayer.strength.base < config.attributeMaximum then
 			path.strengthProgress = (path.strengthProgress - 1)
 			tes3.modStatistic{reference=tes3.player,attribute=0,value=1}
 			tes3.messageBox("Your strength has improved.")
 		end
-	elseif skill == 16 or skill == 13 or skill == 9 or skill == 18 then --intelligence
+	elseif tes3.attributeName[tes3.getSkill(skill).attribute] == "strength" then 
+		path.strengthProgress = (path.strengthProgress + amount)
+		if path.strengthProgress >= 1.0 and tes3.mobilePlayer.strength.base < config.attributeMaximum then
+			path.strengthProgress = (path.strengthProgress - 1)
+			tes3.modStatistic{reference=tes3.player,attribute=0,value=1}
+			tes3.messageBox("Your strength has improved.")
+		end
+	elseif tes3.attributeName[tes3.getSkill(skill).attribute] == "intelligence" then
 		path.intelligenceProgress = (path.intelligenceProgress + amount)
 		if path.intelligenceProgress >= 1.0 and tes3.mobilePlayer.intelligence.base < config.attributeMaximum then
 			path.intelligenceProgress = (path.intelligenceProgress - 1)
 			tes3.modStatistic{reference=tes3.player,attribute=1,value=1}
 			tes3.messageBox("Your intelligence has improved.")
 		end
-	elseif skill == 11 or skill == 10 or skill == 14 or skill == 15 then --willpower
+	elseif tes3.attributeName[tes3.getSkill(skill).attribute] == "willpower" then
 		path.willpowerProgress = (path.willpowerProgress + amount)
 		if path.willpowerProgress >= 1.0 and tes3.mobilePlayer.willpower.base < config.attributeMaximum  then
 			path.willpowerProgress = (path.willpowerProgress - 1)
 			tes3.modStatistic{reference=tes3.player,attribute=2,value=1}
 			tes3.messageBox("Your willpower has improved.")
 		end
-	elseif skill == 0 or skill == 21 or skill == 23 or skill == 19 then --agility
+	elseif tes3.attributeName[tes3.getSkill(skill).attribute] == "agility" then
 		path.agilityProgress = (path.agilityProgress + amount)
 		if path.agilityProgress >= 1.0 and tes3.mobilePlayer.agility.base < config.attributeMaximum then
 			path.agilityProgress = (path.agilityProgress - 1)
 			tes3.modStatistic{reference=tes3.player,attribute=3,value=1}
 			tes3.messageBox("Your agility has improved.")
 		end
-	elseif skill == 8 or skill == 26 or skill == 22 or skill == 17 then --speed
+	elseif tes3.attributeName[tes3.getSkill(skill).attribute] == "speed" then
 		path.speedProgress = (path.speedProgress + amount)
 		if path.speedProgress >= 1.0 and tes3.mobilePlayer.speed.base < config.attributeMaximum then
 			path.speedProgress = (path.speedProgress - 1)
 			tes3.modStatistic{reference=tes3.player,attribute=4,value=1}
 			tes3.messageBox("Your speed has improved.")
 		end
-	elseif skill == 3 or skill == 2 or skill == 7 then --endurance
+	elseif tes3.attributeName[tes3.getSkill(skill).attribute] == "endurance" then
 		path.enduranceProgress = (path.enduranceProgress + amount)
 		if path.enduranceProgress >= 1.0 and tes3.mobilePlayer.endurance.base < config.attributeMaximum then
 			path.enduranceProgress = (path.enduranceProgress - 1)
@@ -126,22 +161,14 @@ local function incrementSkill(skill,amount)
 				tes3.modStatistic{reference=tes3.player,name='health',value=healthGain}
 			end
 		end
-	elseif skill == 12 or skill == 24 or skill == 25 then --personality
+	elseif tes3.attributeName[tes3.getSkill(skill).attribute] == "personality" then
 		path.personalityProgress = (path.personalityProgress + amount)
 		if path.personalityProgress >= 1.0 and tes3.mobilePlayer.personality.base < config.attributeMaximum then
 			path.personalityProgress = (path.personalityProgress - 1)
 			tes3.modStatistic{reference=tes3.player,attribute=6,value=1}
 			tes3.messageBox("Your personality has improved.")
 		end
-	elseif skill == 777 then --luck
-		path.luckProgress = (path.luckProgress + amount)
-		if path.luckProgress >= 1.0 and tes3.mobilePlayer.luck.base < config.attributeMaximum then
-			path.luckProgress = (path.luckProgress - 1)
-			tes3.modStatistic{reference=tes3.player,attribute=7,value=1}
-			--tes3.messageBox("Your luck has improved.")
-		end	
 	end
-	--add support for custom skills?
 end
 
 local function onSkillUp(e)
@@ -149,26 +176,36 @@ local function onSkillUp(e)
 		return
 	end
 	
+	--these should probably just be global to script
 	local path = tes3.player.data.mortLeveling
-	local class = tes3.player.object.class
-	local majors = class.majorSkills
-	local minors = class.minorSkills
-	local threshold = 9 --9 for major/minor, 10 for misc, this affects how many skills levels you up
-	local skillType = "misc"
+	--local majors = tes3.player.object.class.majorSkills
+	--local minors = tes3.player.object.class.minorSkills
+	local threshold = 10 -- test 10 [9 for major/minor, 10 for misc, this affects how many skills levels you up]
+	local skillType = "misc" --default code for skills, will get set to major/minor
+	local skillName = tes3.getSkill(e.skill).name
 	
-	if path == nil then
-		tes3.player.data.mortLeveling = {}
-		path = tes3.player.data.mortLeveling
-		path.strengthProgress = 0
-		path.enduranceProgress = 0
-		path.intelligenceProgress = 0
-		path.willpowerProgress = 0
-		path.luckProgress = 0
-		path.speedProgress = 0
-		path.agilityProgress = 0
-		path.personalityProgress = 0
-		path.miscSkillsRaised = 0
+	if config.skillDecay then
+		local decayTable = tes3.player.data.mortSkillDecay
+		local baseTable = tes3.player.data.mortBaseStats
+		local daysPassed = tes3.worldController.daysPassed.value
+		
+		decayTable[e.skill] = daysPassed
+		
+		for k,_ in pairs(decayTable) do
+		--mwse.log(k .. " " .. tes3.getSkill(k).name .. " " .. decayTable[k] .. " " .. tes3.mobilePlayer.skills[k+1].base .. ">" .. baseTable[k])
+			if decayTable[k] < (daysPassed - config.skillDecayTime) then
+				if (tes3.mobilePlayer.skills[k+1].base > config.skillDecayMin) then
+					decayTable[k] = daysPassed
+					drainSkill(k,-1)
+				elseif config.skillDecayUseBase and (tes3.mobilePlayer.skills[k+1].base > baseTable[k]) then
+					decayTable[k] = daysPassed
+					drainSkill(k,-1)
+				end
+			end
+		end
+		
 	end
+	--print(tes3.player.data.mortSkillDecay[skillName])
 	
 	for _,skill in pairs(majors) do
 		if skill == e.skill then
@@ -203,12 +240,13 @@ local function onSkillUp(e)
 		end
 	end
 	
-	if skillType == "major" or skillType == "minor" then
-		threshold = 9
-	else
-		threshold = 10
-	end
+	-- if skillType == "major" or skillType == "minor" then
+		-- threshold = 10 --test, was 9
+	-- else
+		-- threshold = 10
+	-- end
 	
+	--failsafe if things go crazy and levelupprogress becomes -1, it wraps back around
 	if tes3.mobilePlayer.levelUpProgress > 200 then
 		tes3.mobilePlayer.levelUpProgress = 0
 	end
@@ -223,7 +261,7 @@ local function onSkillUp(e)
 			tes3.mobilePlayer.levelUpProgress = 0
 		else
 			if e.source == "training" then
-				tes3.mobilePlayer.levelUpProgress = -1
+				tes3.mobilePlayer.levelUpProgress = 0 --test, was -1
 			else
 				tes3.mobilePlayer.levelUpProgress = 0
 			end
@@ -239,8 +277,11 @@ local function onSkillUp(e)
 		tes3.streamMusic{path="Special/MW_Triumph.mp3"}
 		
 		--add luck per level
+		--need to add limit here real quick
+		--this is all fucky, luck progress not taken into account, cant add 0.5 luck
 		local luckProgressToAdd = 0.1 * config.luckPerLevel
 		incrementSkill(777,luckProgressToAdd)
+		--tes3.modStatistic{reference=tes3.player,attribute=7,value=luckProgressToAdd}
 		
 		if config.alternateHealthSystem == false then
 			local healthMultiplier = tes3.findGMST(1035)
@@ -248,6 +289,46 @@ local function onSkillUp(e)
 		end
 		tes3.modStatistic{reference=tes3.player,name='health',value=healthToAdd}
 	end
+end
+
+local function setupMULE(e)
+	local path = tes3.player.data.mortLeveling
+	local decayTable = tes3.player.data.mortSkillDecay
+	local baseTable = tes3.player.data.mortBaseStats
+	
+	majors = tes3.player.object.class.majorSkills
+	minors = tes3.player.object.class.minorSkills
+
+	if path == nil then
+		tes3.player.data.mortLeveling = {}
+		path = tes3.player.data.mortLeveling
+		path.strengthProgress = 0
+		path.enduranceProgress = 0
+		path.intelligenceProgress = 0
+		path.willpowerProgress = 0
+		path.luckProgress = 0
+		path.speedProgress = 0
+		path.agilityProgress = 0
+		path.personalityProgress = 0
+		path.miscSkillsRaised = 0
+	end
+	
+	--new feature, keeps compatibility
+	if decayTable == nil then
+		tes3.player.data.mortSkillDecay = {}
+		decayTable = tes3.player.data.mortSkillDecay
+	end
+	
+	-- generate decay base stats only once, store them on the character
+	if baseTable == nil then
+		mwse.log("[MULE] Generating base stats")
+		tes3.player.data.mortBaseStats = {}
+		baseTable = tes3.player.data.mortBaseStats
+		for k,v in pairs(tes3.mobilePlayer.skills) do
+			baseTable[k-1] = v.base
+		end
+	end
+
 end
 
 -- local function changeEndurance(e)
@@ -267,6 +348,7 @@ end
 local function onInitialized()
 	event.register("skillRaised", onSkillUp, {priority = 3000})
 	--event.register("simulate", changeEndurance)
+	event.register("loaded", setupMULE)
 	mwse.log("[MULE] Initialized.")
 end
 event.register("initialized", onInitialized)
@@ -305,6 +387,32 @@ local function registerModConfig()
 								variable = createtableVar("alternateHealthSystem"),
 								defaultSetting = true}
 								
+	categoryMain:createYesNoButton{ label = "Skill Decay", 
+								variable = createtableVar("skillDecay"),
+								defaultSetting = true}
+
+	categoryMain:createYesNoButton{ label = "Display Skill Decay Messagebox", 
+								variable = createtableVar("skillDecayMessage"),
+								defaultSetting = true}
+								
+	categoryMain:createYesNoButton{ label = "Use base stats as decay minimum (only valid on new characters)", 
+								variable = createtableVar("skillDecayUseBase"),
+								defaultSetting = false}
+								
+	categoryMain:createSlider{ label = "Days of no use before a skill decays",
+							variable = createtableVar("skillDecayTime"),
+							min = 1,
+							max = 50,
+							jump = 15,
+							defaultSetting = 15}
+							
+	categoryMain:createSlider{ label = "Minimum a skill can decay to",
+							variable = createtableVar("skillDecayMin"),
+							min = 0,
+							max = 100,
+							jump = 5,
+							defaultSetting = 15}
+							
 	-- categoryMain:createYesNoButton{ label = "[Experimental] State-based Health? Overrides above setting!", 
 								-- variable = createtableVar("stateBasedHealth"),
 								-- defaultSetting = false}
